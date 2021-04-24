590fe33731b653294b55dfd51a05988a59da2c40
Petya02 <sk.petya2002@mail.ru>
2021-04-24 14:17:24 +0600

    Создал компонент ReverseCredit

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as AppRoute } from "./app-route";
 export { default as Menu } from "./menu";
 export { default as Replenishment } from "./replenishment";
 export { default as Advantage } from "./advantage";
+export { default as ReverseCredit } from "./reverse-credit";
